1e7f97d6714de985268aa08e46b56f503874ae53
Tapan patel <108264770+tapangurjar@users.noreply.github.com>
2024-02-03 16:54:12 +0000

    datatype

---
+++ basics/datatype.js
@@ -0,0 +1,15 @@
+let name="tapan"
+let age= 20
+let isloggedIn=false
+
+
+// number =>2 to power 53
+// bigin
+// string =>" "
+// boolean =>true/false
+// null =>standard value
+// undefined => 
+// symbol => unique
+
+console.log(typeof null);  //it is object
+console.log(typeof undefined);  //it is undefined
