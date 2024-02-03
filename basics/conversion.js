31f713d05f1ca8bb3fa1fb77f7bdac8ae9f6a956
Tapan patel <108264770+tapangurjar@users.noreply.github.com>
2024-02-03 17:24:05 +0000

    conversion

---
+++ basics/conversion.js
@@ -0,0 +1,24 @@
+let score="33abd"
+// console.log(typeof score);
+
+let value=Number(score)
+// console.log(typeof value)
+// console.log(value);
+
+// "33"->33
+// "33abc"->NaN
+// true->1; false->0
+
+
+let islogin =0
+let booleanislogin=Boolean(islogin)
+// console.log(booleanislogin)
+
+// 1->true; 0->flase
+// ""->false
+// "hitesh"->true
+
+let somenumber =33
+let stringnumber=String(somenumber)
+// console.log(stringnumber)
+// console.log(typeof stringnumber)
