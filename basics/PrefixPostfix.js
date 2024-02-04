e751b8f909d6e55a3e4db3b79bae3c34e0994378
Tapan patel <108264770+tapangurjar@users.noreply.github.com>
2024-02-04 12:35:45 +0000

    prefixpostfix

---
+++ basics/PrefixPostfix.js
@@ -0,0 +1,16 @@
+// let x=2
+// let y=x++
+// console.log(y)
+// console.log(x)
+
+let x=2
+let y=++x
+// console.log(x);
+// console.log(y);
+
+let user_one={
+    email: "tapanptaelil.com",
+    uid: "1234"
+}
+console.log(user_one);
+console.log(typeof user_one);
