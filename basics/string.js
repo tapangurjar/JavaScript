972f4d24abdcd62627d59e9083f3d4dfab8a4e42
Tapan patel <108264770+tapangurjar@users.noreply.github.com>
2024-02-07 14:10:59 +0000

    string

---
+++ basics/string.js
@@ -0,0 +1,42 @@
+const name="tapan"
+const count=10
+// console.log(name+count+"value")
+
+// console.log(`hello my name is ${name} and my count is ${count}`);
+
+
+const gamename=new String('flappybird')
+
+
+// console.log(gamename[3]);
+//to get the value with index
+
+
+// console.log(gamename.length);
+//to get the length of the string 
+
+// console.log(gamename.toUpperCase());
+//to convert the string in the uppercase
+
+
+// console.log(gamename.charAt(6));
+//to get the charcter with the hlep of index
+
+
+// console.log(gamename.indexOf('i'));
+//to get the index value of chracter
+
+
+const newgamename=gamename.substring(0,4)
+// console.log(newgamename);
+
+
+const anotherstring=gamename.slice(-8,4)
+// console.log(anotherstring);
+
+const newstring="   Tapan "
+console.log(newstring);
+console.log(newstring.trim());
+
+const new1=new String("flappy-bird-game")
+console.log(new1.split('a'));
