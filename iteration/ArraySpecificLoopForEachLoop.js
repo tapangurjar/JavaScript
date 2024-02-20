2b7807952708729a3e8ce3d64cd0305ee4db1db7
Tapan patel <108264770+tapangurjar@users.noreply.github.com>
2024-02-20 17:04:11 +0000

    for each loop

---
+++ iteration/ArraySpecificLoopForEachLoop.js
@@ -0,0 +1,42 @@
+// for each loop
+
+const coding=["js","ruby","java","py","cpp"]
+//in foreach loop we cannot use the name of function in beacuse it is a callback function
+//we can use the arrow function and simple function
+coding.forEach(function(val){
+    // console.log(val);
+})
+coding.forEach((val)=>{
+    // console.log(val);
+})
+
+//also we can call the value of array through this way
+function printme(item){
+    // console.log(item);
+}
+coding.forEach(printme)
+
+const mycoding=
+[
+    {
+        languageName:"javascript",
+        fileextension:"js"
+    },
+    {
+        languageName:"python",
+        fileextension:"py"
+    },
+    {
+        languageName:"java",
+        fileextension:"java"
+    },
+    {
+        languageName:"html",
+        fileextension:"html"
+    }
+]
+
+mycoding.forEach((item)=>{
+    console.log(item.languageName);
+    console.log(item.fileextension);
+})
