5990f5a6d490023ec3053d26a44bc00ee728241e
Tapan patel <108264770+tapangurjar@users.noreply.github.com>
2024-02-20 17:06:01 +0000

    while loop

---
+++ iteration/whileLoop.js
@@ -0,0 +1,20 @@
+//while loop  and do-while loop
+// index=0
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`);
+//     index=index+2  
+// }
+ 
+// let myarray =["tapan","ironman","superman"]
+// let arr =0
+// while (arr<myarray.length){
+//     console.log(`value is ${myarray[arr]}`);
+//     arr=arr+1
+// }
+
+
+let score =11
+do{
+    console.log(`score is ${score}`);
+    score++
+}while(score<=10)
